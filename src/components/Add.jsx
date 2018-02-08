b9c47b14ecfcbb62c8a4b0b12d8a54a8cf045cba
JFenderson <fenderson.joseph@gmail.com>
2018-02-08 16:59:47 -0600

    add.jsx

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Add(props) {
     return(
         <div>
-        <input type="text" onChange={(e)=> this.onChange}/>
+        <input type="text" onChange={(e)=> { props.onChange(e.target.value); } } />
         <button onClick={props.onAdd}>submit</button>
         </div>
     )
